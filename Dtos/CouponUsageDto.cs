380752df4a79942624297d83a1d24c3e7ae3d7dd
Daniel_XD <danielmontiel1600@gmail.com>
2024-06-18 15:17:28 -0500

    modificacion en el DTo de UsageCoupon

--- Dtos/CouponUsageDto.cs
+++ Dtos/CouponUsageDto.cs
@@ -7,7 +7,6 @@ namespace Coupons.Dtos
 {
     public class CouponUsageDto
     {
-        public int CouponId { get; set; }
         public int UserId { get; set; }
         public DateTime UsageDate { get; set; }
         public Double TransactionAmount  { get; set; }
